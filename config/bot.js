d5f586b7341be5fd8b7164627df1b50d46de2881
kimssamu <72490377+kimssamu@users.noreply.github.com>
2021-01-10 20:23:09 +0800

    Minor changes

--- config/bot.js
+++ config/bot.js
@@ -10,7 +10,7 @@ module.exports = {
     discord: {
         token: 'process.env.token',
         prefix: '`',
-        activity: 'ACTIVITY',
+        activity: 'with you',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
