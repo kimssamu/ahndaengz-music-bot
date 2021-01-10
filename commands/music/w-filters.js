caa25666204a165f21c8b9c76b5c14573cec6252
kimssamu <72490377+kimssamu@users.noreply.github.com>
2021-01-10 21:58:07 +0800

    Minor changes

--- commands/music/w-filters.js
+++ commands/music/w-filters.js
@@ -20,8 +20,7 @@ module.exports = {
 
         message.channel.send({
             embed: {
-                color: 'ORANGE',
-                footer: { text: 'This bot uses a Github project made by Zerio (ZerioDev/Music-bot)' },
+                color: 'BLUE',
                 fields: [
                     { name: 'Filters', value: filtersStatuses[0].join('\n'), inline: true },
                     { name: '** **', value: filtersStatuses[1].join('\n'), inline: true },
