299821f677b8aca6c66c5a8d0f74f6d807aacbd5
kimssamu <72490377+kimssamu@users.noreply.github.com>
2021-01-23 22:33:28 +0800

    Base file

---
+++ commands/info/stats.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+var os = require('os')
+
+
+module.exports = {
+    name: 'stats',
+    aliases: ['s'],
+    category: 'Core',
+    utilisation: '{prefix}help <command name>',
+
+    execute(client, message, args) {
+
+const arr = [1, 2, 3, 4, 5, 6, 9, 7, 8, 9, 10];
+arr.reverse();
+const used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+}};
