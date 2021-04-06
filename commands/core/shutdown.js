bec7981f636e10cac0a8715a1e8d6a35fb7f86fc
kimssamu <72490377+kimssamu@users.noreply.github.com>
2021-04-06 15:46:58 +0800

    Base file

---
+++ commands/core/shutdown.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'shutdown',
+    aliases: ['sd', 'fsd'],
+    category: 'Core',
+    utilisation: '{prefix}shutdown',
+
+    execute(client, message, args) {
+
+        message.channel.send("Shutting down...").then(() => {
+            client.destroy();
+            }};
+        });
